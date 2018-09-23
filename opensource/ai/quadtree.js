57bbe4bf30988a3968f30484f76f976dcfc60fa6
Josh Sideris <josh.sideris@gmail.com>
2018-09-23 08:34:05 -0400

    Quadtree implementation.

--- opensource/ai/quadtree.js
+++ opensource/ai/quadtree.js
@@ -9,6 +9,7 @@ class QuadTree{
 		this.height = height;
 		this.depth = depth;
 		this.coords = []; // Eg. {x: 10, y: 15};
+		this.count = 0;
 
 		this.children = null;
 		if(this.depth > 0){
@@ -25,15 +26,30 @@ class QuadTree{
 	 * @param {Number} x X coordinate of the new point to insert.
 	 * @param {Number} y Y coordinate of the new point to insert.
 	 */
-	insertCoord(x, y){
-		throw "NOT IMPLEMENTED";
+	insertCoord(wp){
+		this.count++;
+		if(this.children){
+			if(wp.x < this.centerX){
+				if(wp.y < this.centerY) this.children.tl.insertCoord(wp);
+				else this.children.bl.insertCoord(wp);
+			}
+			else{
+				if(wp.y < this.centerY) this.children.tr.insertCoord(wp);
+				else this.children.br.insertCoord(wp);
+			}
+		}
+		else this.coords.push(wp);
 	}
     getRandomCoord(){
 		if(this.children){ //Choose a random child THAT HAS A COORDINATE IN IT and call getRandomCoord on it.
-			throw "NOT IMPLEMENTED";
+			if(!this.count) return null;
+			let childrenWithWaypoints = Object.keys(this.children).map(k=>this.children[k]).filter(n=>n.count != 0);
+			return childrenWithWaypoints[Math.floor(Math.random() * childrenWithWaypoints.length)].getRandomCoord();
 		}
 		else{ //Choose a random coordinate from this.coords.
 			return this.coords[Math.floor(Math.random() * this.coords.length)];
 		}
 	}
 }
+
+module.exports = QuadTree;
